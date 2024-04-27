0e9226e6b05f3e78a178ab647a05cd0a220cd698
IamPritamPaul <paul.pritamg3@gmail.com>
2024-04-27 19:00:39 +0530

    new methods added

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ Written by Pritam Paul (@IamPritamPaul)
 
 clone the repo
 install python
-then requirements.txt by this command -> pip install -r .\requirements.txt
-then run the python file nsp-mock-server.py
+then install requirements.txt by this command -> pip install -r .\requirements.txt
+then run the python file nsp-mock-server.py <br>
+and open the link in browser to get the list of member details <br>
+http://127.0.0.1:5000/members <br>
+http://127.0.0.1:5000/members/<roll_number>/
--- nsp-mock-server.py
+++ nsp-mock-server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 
 
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 members_data = json.load(open('nsp_members.json','r'))
 
-@app.route("/members", methods=["GET"])
+
+
+@app.route("/members", methods=["https://docs.google.com/spreadsheets/d/1sweUf_oFxKinoJQaK5U6CKVakNRD-j_xx6jkIW58TDk/edit#gid=0GET"])
 def get_members():
     return jsonify(members_data)
 
@@ -17,6 +19,30 @@ def get_member(member_id):
         if member["id"] == member_id:
             return jsonify(member)
     return jsonify({"message": f"Member with ID '{member_id}' not found"}), 404
+
+
+@app.route('/update/<id>', methods=['PUT'])
+def update_resource(id):
+    updated_member=request.get_json()
+    b=False
+    for member in members_data:
+        if member["id"]==id:
+            for key in member.keys():
+                member[key]=updated_member[key]
+            b=True
+    if b==False:
+        return "Invalid request!", 400
+    return "Resource updated successfully!", 200
+
+
+@app.route('/new_member/<id>', methods=['POST'])
+def post_new_member(id):
+    new_member=request.get_json()
+    for member in members_data:
+        if id==member["id"]:
+            return "Member Already Exists!!", 400
+    members_data.append(new_member)
+    return "New member added successfully!!", 201
 
 
 
